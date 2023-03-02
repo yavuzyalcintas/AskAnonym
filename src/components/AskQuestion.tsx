0ce35610657679c7f7bfac1b333c8586305534aa
Yafeshan Unal <yafeshan01@gmail.com>
2023-03-02 12:41:02 +0300

    feat: soru ekleme bildirimi

--- src/components/AskQuestion.tsx
+++ src/components/AskQuestion.tsx
@@ -7,91 +7,91 @@ import React, { useCallback, useEffect, useState } from "react";
 import Button from "./common/button/Button";
 
 interface AskQuestionProps {
-  username: string;
-  topic?: Topic;
+	username: string;
+	topic?: Topic;
 }
 
 function AskQuestion({ username, topic }: AskQuestionProps) {
-  const supabase = useSupabaseClient();
-  const [question, setQuestion] = useState<string>("");
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [ownerUserId, setOwnerUserId] = useState<string>("");
+	const supabase = useSupabaseClient();
+	const [question, setQuestion] = useState<string>("");
+	const [isLoading, setIsLoading] = useState<boolean>(false);
+	const [ownerUserId, setOwnerUserId] = useState<string>("");
 
-  const user = useUser();
-  const isOwnerUser = user && user.user_metadata.username === username;
+	const user = useUser();
+	const isOwnerUser = user && user.user_metadata.username === username;
 
-  const ownerUser = useCallback(async () => {
-    const { data: ownerUser } = await supabase
-      .from("profiles")
-      .select("*")
-      .eq("username", username)
-      .single();
+	const ownerUser = useCallback(async () => {
+		const { data: ownerUser } = await supabase
+			.from("profiles")
+			.select("*")
+			.eq("username", username)
+			.single();
 
-    setOwnerUserId(ownerUser?.id!);
-  }, [username, supabase]);
+		setOwnerUserId(ownerUser?.id!);
+	}, [username, supabase]);
 
-  useEffect(() => {
-    ownerUser();
-  }, [ownerUser]);
+	useEffect(() => {
+		ownerUser();
+	}, [ownerUser]);
 
-  async function createQuestion() {
-    if (question === "") {
-      return;
-    }
-    setIsLoading(true);
+	async function createQuestion() {
+		if (question === "") {
+			alert("Lütfen Geçerli Bir Soru Giriniz");
+		}
+		setIsLoading(true);
 
-    const { error } = await supabase.from("questions").insert({
-      question: question!,
-      user_id: ownerUserId!,
-      topic_id: topic?.id,
-    });
+		const { error } = await supabase.from("questions").insert({
+			question: question!,
+			user_id: ownerUserId!,
+			topic_id: topic?.id,
+		});
 
-    if (!error) {
-      setQuestion("");
-    }
-    setIsLoading(false);
-  }
+		if (!error) {
+			setQuestion("");
+			alert("Soru Başarıyla Gönderildi");
+		}
+		setIsLoading(false);
+	}
 
-  return (
-    <>
-      {!isOwnerUser && (
-        <div className="bg-gray-50 px-4 py-6 sm:px-6">
-          <div className="flex space-x-3">
-            <div className="min-w-0 flex-1">
-              <div>
-                <textarea
-                  name="question"
-                  value={question}
-                  onChange={(e) => setQuestion(e.target.value)}
-                  rows={2}
-                  className="block w-full rounded-md border-gray-300 shadow-sm focus:border-purple-500 focus:ring-purple-500 sm:text-sm"
-                  placeholder={
-                    "Ask anonymous question " +
-                    (topic ? `in #${topic?.name}` : "")
-                  }
-                />
-                <label htmlFor="question" className="text-xs text-gray-500">
-                  * questions will be published after user{" "}
-                  <span className="text-lime-500 font-bold">approval</span>{" "}
-                  process.
-                </label>
-              </div>
-              <div className="mt-3 flex justify-end">
-                <Button
-                  size="small"
-                  startIcon={<ChatBubbleLeftIcon className="w-5 h-5" />}
-                  isLoading={isLoading}
-                  onClick={() => createQuestion()}
-                >
-                  Ask Question
-                </Button>
-              </div>
-            </div>
-          </div>
-        </div>
-      )}
-    </>
-  );
+	return (
+		<>
+			{!isOwnerUser && (
+				<div className="bg-gray-50 px-4 py-6 sm:px-6">
+					<div className="flex space-x-3">
+						<div className="min-w-0 flex-1">
+							<div>
+								<textarea
+									name="question"
+									value={question}
+									onChange={(e) => setQuestion(e.target.value)}
+									rows={2}
+									className="block w-full rounded-md border-gray-300 shadow-sm focus:border-purple-500 focus:ring-purple-500 sm:text-sm"
+									placeholder={
+										"Ask anonymous question " +
+										(topic ? `in #${topic?.name}` : "")
+									}
+								/>
+								<label htmlFor="question" className="text-xs text-gray-500">
+									* questions will be published after user{" "}
+									<span className="text-lime-500 font-bold">approval</span>{" "}
+									process.
+								</label>
+							</div>
+							<div className="mt-3 flex justify-end">
+								<Button
+									size="small"
+									startIcon={<ChatBubbleLeftIcon className="w-5 h-5" />}
+									isLoading={isLoading}
+									onClick={() => createQuestion()}>
+									Ask Question
+								</Button>
+							</div>
+						</div>
+					</div>
+				</div>
+			)}
+		</>
+	);
 }
 
 export default AskQuestion;
