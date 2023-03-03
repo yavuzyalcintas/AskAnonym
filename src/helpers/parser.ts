6932fd86768493bdcf8817e942043aae942de478
Yavuz Yalçıntaş <myavuzyalcintas@gmail.com>
2023-03-04 02:09:56 +0300

    trimmer

--- src/helpers/parser.ts
+++ src/helpers/parser.ts
@@ -3,14 +3,13 @@ import { z } from "zod";
 export const specialCharacterParse = (str: string) => z
   .string()
   .min(1)
+  .trim()
   .regex(/^[a-zA-Z]+$/)
   .safeParse(str);
 
 
 export const generalParse = (str: string) => z
   .string()
-  .min(1)
   .max(150)
-  .trim()
   .regex(/^[a-zA-ZıİüÜöÖçÇşŞğĞ1-9!()"?.\ ]+$/)
   .safeParse(str);
