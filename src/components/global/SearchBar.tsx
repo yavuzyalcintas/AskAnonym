e243fdea70732c55cf6cc2614eb062a77225c18d
Ahmet ilhan <ahmetilhan.dev@gmail.com>
2023-03-06 20:44:12 +0300

    empty control added, search performance improved (for network, history of search cleaned)

--- src/components/global/SearchBar.tsx
+++ src/components/global/SearchBar.tsx
@@ -18,15 +18,18 @@ function SearchBar() {
   }
   let timer: ReturnType<typeof setTimeout>;
   const fetchSuggestions = (val: string) => {
-    clearTimeout(timer);
-    timer = setTimeout(async () => {
-      const { data } = await supabase
-        .from("profiles")
-        .select("username,avatar_url")
-        .ilike("username", `%${val}%`)
-        .limit(5);
-      setPersonList(data ? data : []);
-    }, 1000);
+    setPersonList([]);
+    if (val.length >= 3) {
+      clearTimeout(timer);
+      timer = setTimeout(async () => {
+        const { data } = await supabase
+          .from("profiles")
+          .select("username,avatar_url")
+          .ilike("username", `%${val}%`)
+          .limit(5);
+        setPersonList(data ? data : []);
+      }, 1000);
+    }
   };
   return (
     <div className="flex items-center px-6 md:mx-auto md:max-w-3xl lg:mx-0 lg:max-w-none xl:px-0">
@@ -49,32 +52,34 @@ function SearchBar() {
                 fetchSuggestions(e.target.value);
               }}
             ></Combobox.Input>
-            <Combobox.Options className="absolute w-full rounded-md border border-gray-300 bg-white py-2 px-3">
-              {personList.map(person => (
-                <Combobox.Option
-                  className="border-b border-gray-100 py-2"
-                  key={person["username"]}
-                  value={person["username"]}
-                >
-                  <div className="flex items-center space-x-4">
-                    <div className="shrink-0">
-                      <Image
-                        className="h-8 w-8 rounded-full"
-                        src={person.avatar_url}
-                        alt={person.username}
-                        width={32}
-                        height={32}
-                      />
+            {personList.length ? (
+              <Combobox.Options className="absolute w-full rounded-md border border-gray-300 bg-white py-2 px-3">
+                {personList.map(person => (
+                  <Combobox.Option
+                    className="border-b border-gray-100 py-2"
+                    key={person["username"]}
+                    value={person["username"]}
+                  >
+                    <div className="flex items-center space-x-4">
+                      <div className="shrink-0">
+                        <Image
+                          className="h-8 w-8 rounded-full"
+                          src={person.avatar_url}
+                          alt={person.username}
+                          width={32}
+                          height={32}
+                        />
+                      </div>
+                      <div className="min-w-0 flex-1">
+                        <p className="cursor-pointer text-sm font-medium text-gray-900">
+                          {person.username}
+                        </p>
+                      </div>
                     </div>
-                    <div className="min-w-0 flex-1">
-                      <p className="cursor-pointer text-sm font-medium text-gray-900">
-                        {person.username}
-                      </p>
-                    </div>
-                  </div>
-                </Combobox.Option>
-              ))}
-            </Combobox.Options>
+                  </Combobox.Option>
+                ))}
+              </Combobox.Options>
+            ) : null}
           </Combobox>
         </div>
       </div>
