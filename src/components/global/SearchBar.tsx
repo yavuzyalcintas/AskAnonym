7aadbdf019e5bed22d1c0ae1279abedc4ef1e3e1
Ahmet İlhan <60369989+ahmetilhn@users.noreply.github.com>
2023-03-07 20:53:47 +0300

    SearchBar Update (Debounce, performance, UI improve) (#28)
    * 🌴 debounce added (for searchbar)
    
    * empty control added, search performance improved (for network, history of search cleaned)
    
    * 💄 SearchBar improved
    
    * 👽 query limit updated
    
    * 🦠 lint applied
    
    * Update SearchBar.tsx
    
    * console removed
    
    ---------
    
    Co-authored-by: Uğur Oruç <uoruc5@hotmail.com>

--- src/components/global/SearchBar.tsx
+++ src/components/global/SearchBar.tsx
@@ -5,28 +5,46 @@ import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { useSupabaseClient } from "@supabase/auth-helpers-react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
+import Loader from "../common/loader/Loader";
+let timer: ReturnType<typeof setTimeout>;
+const queryLimit = 1;
 function SearchBar() {
   const supabase = useSupabaseClient();
   const router = useRouter();
   const [personList, setPersonList] = useState<
     { username: any; avatar_url: any }[]
   >([]);
-
+  const [query, setQuery] = useState("");
+  const [isLoading, setLoading] = useState(false);
   function goProfile(username: string) {
     router.push(`/${username}`);
   }
-
-  async function setSearchQuery(value: string) {
-    const { data } = await supabase
-      .from("profiles")
-      .select("username,avatar_url")
-      .ilike("username", `%${value}%`)
-      .limit(5);
-    setPersonList(data ? data : []);
-  }
-
+  const fetchSuggestions = (val: string) => {
+    setLoading(true);
+    setPersonList([]);
+    if (val.length >= queryLimit) {
+      clearTimeout(timer);
+      timer = setTimeout(async () => {
+        try {
+          const { data } = await supabase
+            .from("profiles")
+            .select("username,avatar_url")
+            .ilike("username", `%${val}%`)
+            .limit(5);
+          setPersonList(data ? data : []);
+        } catch (error) {
+          throw new Error("Error fetching suggestions");
+        } finally {
+          setLoading(false);
+        }
+      }, 1000);
+    }
+  };
+  useEffect(() => {
+    fetchSuggestions(query);
+  }, [query]);
   return (
     <div className="flex items-center px-6 md:mx-auto md:max-w-3xl lg:mx-0 lg:max-w-none xl:px-0">
       <div className="w-full">
@@ -42,38 +60,48 @@ function SearchBar() {
           </div>
           <Combobox value="" onChange={goProfile}>
             <Combobox.Input
-              placeholder="Search"
+              placeholder="Search (min 1 char)"
               className="block w-full rounded-md border border-gray-300 bg-white py-2 pl-10 pr-3 text-sm placeholder:text-gray-500 focus:border-purple-700 focus:text-gray-900 focus:outline-none focus:ring-1 focus:ring-purple-700 focus:placeholder:text-gray-400 sm:text-sm"
               onChange={e => {
-                setSearchQuery(e.target.value);
+                setQuery(e.target.value);
               }}
             ></Combobox.Input>
-            <Combobox.Options className="absolute w-full rounded-md border border-gray-300 bg-white py-2 px-3">
-              {personList.map(person => (
-                <Combobox.Option
-                  className="border-b border-gray-100 py-2"
-                  key={person["username"]}
-                  value={person["username"]}
-                >
-                  <div className="flex items-center space-x-4">
-                    <div className="shrink-0">
-                      <Image
-                        className="h-8 w-8 rounded-full"
-                        src={person.avatar_url}
-                        alt={person.username}
-                        width={32}
-                        height={32}
-                      />
+            {query.length >= queryLimit && (
+              <Combobox.Options className="absolute w-full content-center rounded-md border border-gray-300 bg-white py-2 px-3">
+                {personList.map(person => (
+                  <Combobox.Option
+                    className="border-b border-gray-100 py-2"
+                    key={person["username"]}
+                    value={person["username"]}
+                  >
+                    <div className="flex items-center space-x-4">
+                      <div className="shrink-0">
+                        <Image
+                          className="h-8 w-8 rounded-full"
+                          src={person.avatar_url}
+                          alt={person.username}
+                          width={32}
+                          height={32}
+                        />
+                      </div>
+                      <div className="min-w-0 flex-1">
+                        <p className="cursor-pointer text-sm font-medium text-gray-900">
+                          {person.username}
+                        </p>
+                      </div>
                     </div>
-                    <div className="min-w-0 flex-1">
-                      <p className="cursor-pointer text-sm font-medium text-gray-900">
-                        {person.username}
-                      </p>
+                  </Combobox.Option>
+                ))}
+                <div className="flex w-full justify-center">
+                  {!isLoading && personList.length === 0 && (
+                    <div className="py-2 text-center text-sm">
+                      No results found
                     </div>
-                  </div>
-                </Combobox.Option>
-              ))}
-            </Combobox.Options>
+                  )}
+                  {isLoading && <Loader color="#000" size={28} />}
+                </div>
+              </Combobox.Options>
+            )}
           </Combobox>
         </div>
       </div>
