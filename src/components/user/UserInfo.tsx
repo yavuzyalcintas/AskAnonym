513cf95290ff39b0ef0c4fa6c3550075df7159f8
Reşat YILDIZ <resat328@gmail.com>
2023-03-09 15:09:54 +0300

    Fix code according to eslint rules

--- src/components/user/UserInfo.tsx
+++ src/components/user/UserInfo.tsx
@@ -3,11 +3,23 @@
 import { CalendarIcon, MapIcon } from "@heroicons/react/24/outline";
 import React, { FC, ReactNode } from "react";
 
+const UserInfoText: FC<{
+  text: string;
+  icon?: ReactNode;
+}> = ({ text, icon }) => {
+  return (
+    <span className="flex items-center justify-start gap-2 text-xs text-gray-500">
+      {!!icon && icon}
+      <p className="m-0 mt-0.5 p-0">{text}</p>
+    </span>
+  );
+};
+
 export const UserInfo = () => {
   return (
     <>
       <hr className="my-2" />
-      <div className="my-1 py-1 block">
+      <div className="my-1 block py-1">
         <div className="w-full">
           <UserInfoText
             text="I`m a software developer. I`m interesting in ReactJS, NextJS and
@@ -15,29 +27,17 @@ export const UserInfo = () => {
               their info."
           />
         </div>
-        <div className="flex justify-start items-center gap-3 mt-0.5">
+        <div className="mt-0.5 flex items-center justify-start gap-3">
           <UserInfoText
             text={"05.03.2023"}
-            icon={<CalendarIcon className="w-4 h-4" />}
+            icon={<CalendarIcon className="h-4 w-4" />}
           />
           <UserInfoText
             text={"Istanbul/TURKIYE"}
-            icon={<MapIcon className="w-4 h-4" />}
+            icon={<MapIcon className="h-4 w-4" />}
           />
         </div>
       </div>
     </>
-  );
-};
-
-const UserInfoText: FC<{
-  text: string;
-  icon?: ReactNode;
-}> = ({ text, icon }) => {
-  return (
-    <span className="flex justify-start gap-2 text-xs text-gray-500 items-center">
-      {!!icon && icon}
-      <p className="p-0 m-0 mt-0.5">{text}</p>
-    </span>
   );
 };
