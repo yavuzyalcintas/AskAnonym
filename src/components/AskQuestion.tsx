a0dc4affe34ceaeea7afb60b0bed0b245bceb408
Yavuz Yalçıntaş <myavuzyalcintas@gmail.com>
2023-03-12 13:43:58 +0300

    max char 1000

--- src/components/AskQuestion.tsx
+++ src/components/AskQuestion.tsx
@@ -82,7 +82,7 @@ function AskQuestion({ username, topic }: AskQuestionProps) {
                     setQuestionContentLength(e.target.value.length);
                   }}
                   rows={2}
-                  maxLength={250}
+                  maxLength={1000}
                   className="block w-full rounded-md border-gray-300 shadow-sm focus:border-purple-500 focus:ring-purple-500 sm:text-sm"
                   placeholder={
                     "Ask anonymous question " +
@@ -97,7 +97,7 @@ function AskQuestion({ username, topic }: AskQuestionProps) {
               </div>
               <div className="flex justify-end">
                 <label className="text-xs font-bold text-red-600">
-                  {questionContentLength}/250
+                  {questionContentLength}/1000
                 </label>
               </div>
               <div className="mt-3 flex justify-end">
