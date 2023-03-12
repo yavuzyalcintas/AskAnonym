ea55434ccf9cd43aaa2f8f0e76f0d551c0a4417f
Mahmut Can ÇİFÇİ <mhcifci@gmail.com>
2023-03-12 23:07:57 +0300

    profile avatar + post avatar size changed

--- src/components/global/UserCard.tsx
+++ src/components/global/UserCard.tsx
@@ -24,6 +24,11 @@ function UserCard({ profile, variant }: UserCardProps) {
       <div className="shrink-0">
         <div className="relative">
           <Avatar
+            classname={
+              variant === "profile"
+                ? "md:h-[128px] md:w-[128px] sm:h-[64px] sm:w-[64px]"
+                : "md:h-[48px] md:w-[48px] sm:h-[16px] sm:w-[16px]"
+            }
             username={profile.username}
             url={profile.avatar_url}
             size={variant == "profile" ? 128 : 40}
@@ -39,7 +44,7 @@ function UserCard({ profile, variant }: UserCardProps) {
           className={
             (profile.is_verified?.text_color
               ? profile.is_verified?.text_color +
-                " dark:text-purple-400 ml-2sm:ml-4 sm:ml-2"
+                " dark:text-purple-400 ml-2sm:ml-4 sm:ml-1"
               : "  text-purple-700 dark:text-purple-400  ") +
             (variant === "profile"
               ? " text-4xl sm:ml-4 sm:text-[28px] md:text-[42px]"
@@ -52,10 +57,11 @@ function UserCard({ profile, variant }: UserCardProps) {
           {profile.is_verified &&
             (profile.is_verified.type === "leader" ? (
               <Image
+                className="sm:ml-2"
                 src="/images/leader.png"
                 alt="leader"
-                width={variant == "profile" ? 80 : 25}
-                height={variant == "profile" ? 80 : 25}
+                width={variant == "profile" ? 40 : 25}
+                height={variant == "profile" ? 40 : 25}
               />
             ) : (
               <CheckBadgeIcon className={`${badgeClasses} ml-1`} />
