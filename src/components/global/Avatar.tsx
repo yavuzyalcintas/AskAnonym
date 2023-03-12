2d72ecf2cde800642cd50eb6515d8641f724e91d
Mahmut Can ÇİFÇİ <mhcifci@gmail.com>
2023-03-12 23:18:57 +0300

    wrongly deleted

---
+++ src/components/global/Avatar.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import React from "react";
+
+interface AvatarProps {
+  username: string;
+  url: string | null;
+  size: 32 | 40 | 64 | 72 | 128;
+  classname?: any;
+}
+
+function Avatar({ url, size, username, classname }: AvatarProps) {
+  const image = url || "/images/default-avatar.png";
+  return (
+    <Link href={"/" + username}>
+      <div
+        className={classname}
+        style={{
+          backgroundImage: `url(${image})`,
+          // width: size,
+          // height: size,
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+          borderRadius: "50%"
+        }}
+      />
+    </Link>
+  );
+}
+
+export default Avatar;
