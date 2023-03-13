900e3ecd9ed912cf8c9f3fb21bf9ef5da3288e51
eraygundogmus <eraygundogmus@teknasyon.com>
2023-03-13 13:23:56 +0300

    fix: make little ui fixes

--- app/[username]/page.tsx
+++ app/[username]/page.tsx
@@ -84,7 +84,7 @@ export default async function UserProfile({
   return (
     <>
       <div className="min-h-full ">
-        <main className="py-10">
+        <main className="py-4 sm:py-10">
           {/* Page header */}
           <div className="mx-auto max-w-3xl px-4 sm:px-6 md:flex md:items-center md:justify-between md:space-x-5 lg:max-w-7xl lg:px-8">
             <UserCard profile={ownerUser as User} variant="profile" />
